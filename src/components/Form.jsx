8cd80f46430a2d66b8216aa4752315a93ae8f666
fqwerT <viktor.potapov.03@list.ru>
2024-10-22 17:09:16 +0400

    added jewish form heheh

---
+++ src/components/Form.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export const Form = () => {
+  return (
+    <div>
+      <input />
+      <input />
+      <input />
+      <input />
+      <input />
+      <img src="https://images.squarespace-cdn.com/content/v1/5ee500d316a2470c370596d3/1602326295244-T6VS8ZK1U9S2HWESM51X/Untitled-design-5-1.jpg" />
+    </div>
+  );
+};
